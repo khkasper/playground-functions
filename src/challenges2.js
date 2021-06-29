071f34f9d459bb9077ce5fb565ddf5f28faa4d26
Kristiano <kristianokasper@gmail.com>
2021-06-29 20:26:59 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, name) {
+  
+  //Setar array para conter as informações
+  let array = [];
+  
+  //Após o reOrdenar o array final, após o retorno da função, em ordem alfabética
+  arrayTech.sort();
+
+  //Acessar as chaves do array de tecnologias informado
+  for (let index in arrayTech) {
+    //Inserir informações nesta chave 
+    array.push({
+    //Informações inseridas
+    tech: arrayTech[index],
+    name: name,
+    });
+  };
+  //Condicional se vazio ou não
+  if (array == 0) {
+    return 'Vazio!';
+  } else {
+    return array;
+  };
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Kristiano'));
 
 // Desafio 11
 function generatePhoneNumber() {
