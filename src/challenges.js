efc73b95bbc6a3b9a6f68d5f80bc55d9a1e1bdfb
Kristiano <kristianokasper@gmail.com>
2021-06-29 16:47:23 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -6,23 +6,30 @@ function compareTrue(a, b) {
     return false;
   }
 }
+// console.log(compareTrue(true, true))
 
 // Desafio 2
 function calcArea(base, height) {
   let area = (base * height) / 2;
   return area;
 }
+// console.log(calcArea(10, 50))
 
 // Desafio 3
 function splitSentence(sentence) {
   let array = sentence.split(' ');
   return array;
 }
+// console.log(splitSentence('go Trybe'))
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let first = array[0];
+  let last = array[array.length -1];
+  let concat = [first + " " + last];
+  return concat;
 }
+// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
 function footballPoints() {
