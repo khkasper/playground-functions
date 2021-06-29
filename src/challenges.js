cfec3edc0815067dc8dacb201e4ad82fe4f4446c
Kristiano <kristianokasper@gmail.com>
2021-06-29 20:50:33 -0300

    Correçoes 2

--- src/challenges.js
+++ src/challenges.js
@@ -25,11 +25,11 @@ function splitSentence(sentence) {
 // Desafio 4
 function concatName(array) {
   let first = array[0];
-  let last = array[array.length -1];
-  let concat = last + "," + " " + first;
+  let last = array[array.length - 1];
+  let concat = last + '', ' + first;
   return concat;
 }
-// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -40,19 +40,15 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-
-  //Verificar o maior número dentro do array e armazenar essa informação
-  let higherNumber = array[0];
+  let higherNumber = array[0];//Verificar o maior número dentro do array e armazenar essa informação
   for (let i = 1; i < array.length; i += 1) {
     if (array[i] > higherNumber) {
       higherNumber = array[i];
     }
   }
-
-  //Contador para ver quantas vezes o número armazenado anteriormente se repete
-  let counter = 0;
+  let counter = 0;//Contador para ver quantas vezes o número armazenado anteriormente se repete
   for (let i = 0; i < array.length; i += 1) {
-    if (higherNumber == array[i]) {
+    if (higherNumber === array[i]) {
       counter += 1;
     }
   }
@@ -65,9 +61,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let c1M = cat1 - mouse;
   let c2M = cat2 - mouse;
   if (c1M < c2M) {
-    return "cat1";
+    return 'cat1';
   } else if (c1M > c2M) {
-    return "cat2";
+    return 'cat2';
   } else {
     return 'os gatos trombaram e o rato foge';
   }
@@ -77,9 +73,9 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(array) {
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
+    if (array[index] % 3 === 0 && array[index] % 5 !=== 0) {
       array[index] = 'fizz';
-    } else if (array[index] % 3 !== 0 && array[index] % 5 == 0) {
+    } else if (array[index] % 3 !=== 0 && array[index] % 5 === 0) {
       array[index] = 'buzz';
     } else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
       array[index] = 'fizzbuzz';
@@ -98,25 +94,25 @@ function fizzBuzz(array) {
 
 function encode(string) {
 
-  let enc = '';
+let enc = '';
   enc = string.replace(/a/gi, 1);
-  enc = enc.replace(/e/gi, "2");
-  enc = enc.replace(/i/gi, "3");
-  enc = enc.replace(/o/gi, "4");
-  enc = enc.replace(/u/gi, "5");
-  return enc;
+  enc = enc.replace(/e/gi, '2');
+  enc = enc.replace(/i/gi, '3');
+  enc = enc.replace(/o/gi, '4');
+  enc = enc.replace(/u/gi, '5');
+return enc;
 }
 // console.log(encode('hi there!'))
 
 function decode(string) {
 
-  let dec = '';
-  dec = string.replace(/1/gi, "a");
-  dec = dec.replace(/2/gi, "e");
-  dec = dec.replace(/3/gi, "i");
-  dec = dec.replace(/4/gi, "o");
-  dec = dec.replace(/5/gi, "u");
-  return dec;
+let dec = '';
+  dec = string.replace(/1/gi, 'a');
+  dec = dec.replace(/2/gi, 'e');
+  dec = dec.replace(/3/gi, 'i');
+  dec = dec.replace(/4/gi, 'o');
+  dec = dec.replace(/5/gi, 'u);
+return dec;
   
 }
 // console.log(decode('h3 th2r2!'))
