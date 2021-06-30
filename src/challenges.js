caa81866d473fca55bc53b38e039c078344d2229
Kristiano <kristianokasper@gmail.com>
2021-06-30 13:28:34 -0300

    Atualizaçoes para o Lint

--- src/challenges.js
+++ src/challenges.js
@@ -2,51 +2,44 @@
 function compareTrue(a, b) {
   if (a === true && b === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
-// console.log(compareTrue(true, true))
+// console.log(compareTrue(true, false))
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height) / 2;
-  return area;
+  return (base * height) / 2;
 }
 // console.log(calcArea(10, 50))
 
 // Desafio 3
 function splitSentence(sentence) {
-  let array = sentence.split(' ');
-  return array;
+  return sentence.split(' ');
 }
 // console.log(splitSentence('go Trybe'))
 
 // Desafio 4
 function concatName(array) {
-  let first = array[0];
-  let last = array[array.length - 1];
-  let concat = last + '', ' + first;
-  return concat;
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let result = wins * 3 + ties;
-  return result;
+  return wins * 3 + ties;
 }
 // console.log(footballPoints(14, 8))
 
 // Desafio 6
 function highestCount(array) {
-  let higherNumber = array[0];//Verificar o maior número dentro do array e armazenar essa informação
+  let higherNumber = array[0];// Verificar o maior número dentro do array e armazenar essa informação
   for (let i = 1; i < array.length; i += 1) {
     if (array[i] > higherNumber) {
       higherNumber = array[i];
     }
   }
-  let counter = 0;//Contador para ver quantas vezes o número armazenado anteriormente se repete
+  let counter = 0;// Contador para ver quantas vezes o número armazenado anteriormente se repete
   for (let i = 0; i < array.length; i += 1) {
     if (higherNumber === array[i]) {
       counter += 1;
@@ -60,11 +53,13 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let c1M = cat1 - mouse;
   let c2M = cat2 - mouse;
-  if (c1M < c2M) {
+  if (c2M > c1M) {
     return 'cat1';
-  } else if (c1M > c2M) {
+  }
+  if (c1M > c2M) {
     return 'cat2';
-  } else {
+  }
+  if (c1M === c2M) {
     return 'os gatos trombaram e o rato foge';
   }
 }
@@ -73,12 +68,12 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(array) {
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] % 3 === 0 && array[index] % 5 !=== 0) {
+    if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
       array[index] = 'fizz';
-    } else if (array[index] % 3 !=== 0 && array[index] % 5 === 0) {
+    } else if (array[index] % 3 !== 0 && array[index] % 5 === 0) {
       array[index] = 'buzz';
     } else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
-      array[index] = 'fizzbuzz';
+      array[index] = 'fizzBuzz';
     } else {
       array[index] = 'bug!';
     }
@@ -89,31 +84,28 @@ function fizzBuzz(array) {
 
 // Desafio 9
 
-  //https://www.w3schools.com/jsref/jsref_replace.asp
-  //Substituir cada uma dos elementos selecionados ("g" pega todas dentro da string e "i" considera case sensive) por outro.
+// https://www.w3schools.com/jsref/jsref_replace.asp
+// Substituir cada uma dos elementos selecionados ("g" pega todas dentro da string e "i" considera case sensive) por outro.
 
 function encode(string) {
-
-let enc = '';
+  let enc = '';
   enc = string.replace(/a/gi, 1);
   enc = enc.replace(/e/gi, '2');
   enc = enc.replace(/i/gi, '3');
   enc = enc.replace(/o/gi, '4');
   enc = enc.replace(/u/gi, '5');
-return enc;
+  return enc;
 }
 // console.log(encode('hi there!'))
 
 function decode(string) {
-
-let dec = '';
+  let dec = '';
   dec = string.replace(/1/gi, 'a');
   dec = dec.replace(/2/gi, 'e');
   dec = dec.replace(/3/gi, 'i');
   dec = dec.replace(/4/gi, 'o');
-  dec = dec.replace(/5/gi, 'u);
-return dec;
-  
+  dec = dec.replace(/5/gi, 'u');
+  return dec;
 }
 // console.log(decode('h3 th2r2!'))
 
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,23 +1,19 @@
 // Desafio 10
 function techList(arrayTech, name) {
-  
-  //Ordenar o array final em ordem alfabética
-  arrayTech.sort();
-  let array = [];//Setar array para conter as informações
-  for (let index in arrayTech) {//Acessar as chaves do array das "tecnologias" informado
-    array.push({//Inserir informações nesta chave
-    tech: arrayTech[index],//Informações do array informado
-    name: name,//Informações do nome solicitado
-    });
-  };
-  if (array === 0) { //Condicional se vazio
+  arrayTech.sort(); // Ordenar o array em ordem alfabética
+  if (arrayTech.length === 0) { // Verificar se o array informado contém algo
     return 'Vazio!';
-  } else {
-    //Condicional de não vazio
-    return array;
-  };
+  }
+  let array = []; // Setar array para conter as informações
+  for (let index in arrayTech) { // Irá acessar as chaves do array das "tecnologias"
+    array.push({ // Inserir informações nesta chave
+      tech: arrayTech[index], // Informações do array com as tecnologias informadas
+      name: name, // Informações do nome solicitado
+    });
+  }
+  return array; // Condicional de não vazio
 }
-// console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Kristiano'));
+// console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Kristiano'));
 
 // Desafio 11
 function generatePhoneNumber() {
