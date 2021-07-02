c362ba310a984a0a947220c7ea52c3300ab09288
Kristiano <kristianokasper@gmail.com>
2021-07-02 16:55:28 -0300

    Exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,9 +16,29 @@ function techList(arrayTech, name) {
 // console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Kristiano'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0 || array[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores' 
+    }
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    let counter = 0;
+    for (let key of array) {
+      if (key === array[i]) {
+        counter += 1
+      }
+      if (counter >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores'
+      }
+    }
+  }
+  return '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
 }
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -32,7 +52,7 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate() {
-  
+  // Z
 }
 
 module.exports = {
